5960d8270d75bd9a92cad98277df56bfa6bf3742
Joel Okebugwu <108504020+joelyoung2020@users.noreply.github.com>
2022-09-14 10:33:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "eks" {
 
   aws_auth_roles = [
     {
-      rolearn  = "arn:aws:iam::069269258193:role/eksClusterRole"
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
       username = "role1"
       groups   = ["system:masters"]
     },
