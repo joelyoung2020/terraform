377f51996882f0ae3ee6d3aae8016ca4fe1fbebb
Joel Okebugwu <108504020+joelyoung2020@users.noreply.github.com>
2022-10-03 17:32:23 +0100

    Update variables.tf

--- EKS cluster with managed node/variables.tf
+++ EKS cluster with managed node/variables.tf
@@ -2,6 +2,14 @@ variable "region" {
   default = "eu-west-3"
   description = "AWS region"
 }
+variable "access" {
+  default = "AKIARAIGE77IW7AVZ6ZO"
+  description = "AWS access key"
+}
+variable "secret" {
+  default = "GiQcQG2ec1IljvxI0RlWwPZ+W3H67jjk5zhLnzPl"
+  description = "AWS secret key"
+}
 variable "cluster_name" {
   default = "getting-started-eks"
 }
